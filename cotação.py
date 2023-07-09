1c4b4110416d8bb86dd9ed1840d31b7c66d68444
Daniel Antunes <daniel.antunes.nutri@gmail.com>
2023-07-09 17:32:44 -0300

    adicionado tratamento de erros para inputs do tipo int, comentários no código e cotação do iene japonês

--- cotação.py
+++ cotação.py
@@ -4,12 +4,12 @@ from datetime import datetime
 print('     SISTEMA DE COTAÇÃO DE MOEDAS')
 data_atual = str(datetime.today())
 print(f'Data atual: {data_atual}')
-print('=' * 40)
+print('=' * 50)
 
 requisicao = requests.get(
     'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BRL-USD,BRL-EUR,USD-EUR,EUR-USD,JPY-BRL,BRL-JPY').json()
-print(requisicao)
-lista_de_moedas = ['USDBRL', 'EURBRL', 'BRLUSD', 'BRLEUR', 'USDEUR', 'EURUSD']
+# print(requisicao)
+lista_de_moedas = ['USDBRL', 'EURBRL', 'JPYBRL']
 dic_moedas = {1: 'USD', 2: 'BRL', 3: 'EUR', 4: 'JPY'}
 while True:
     print('Escolha uma opção: ')
@@ -17,20 +17,25 @@ while True:
                                 '[2] - Converter moeda\n'
                                 '[3] - Encerrar\n'
                                 'Sua escolha: '))
+    print('-' * 50)
     if escolha_usuario == '1':
         while True:
             try:
                 escolha_cotacao = int(input('Quer ver a cotação de qual moeda?\n'
                                             '[1] - Dólar\n'
                                             '[2] - Euro\n'
-                                            '[3] - Encerrar\n'
+                                            '[3] - Iene japonês\n'
+                                            '[4] - Encerrar\n'
                                             'Sua escolha: '))
-                if escolha_cotacao == 3:
+                print('-' * 50)
+                if escolha_cotacao == 4:
                     break
-                elif escolha_cotacao != 1 and escolha_cotacao != 2:
+                elif escolha_cotacao not in (1, 2, 3):
                     print('Opção inválida.')
                 else:
+                    # Pega a cotação da moeda escolhida
                     moeda = lista_de_moedas[escolha_cotacao - 1]
+                    print('1 real representa: ')
                     print(f'Cotação {moeda}: {requisicao[f"{moeda}"]["bid"]}')
                     print('Flutuação diária:')
                     print(f'Mínima: {requisicao[moeda]["low"]}\n'
@@ -39,39 +44,50 @@ while True:
                 print('Erro no tipo ou valor.')
             except Exception as erro:
                 print(f'Erro: {erro.__cause__}')
-            print('-' * 40)
+            print('-' * 50)
     elif escolha_usuario == '2':
         while True:
-            valor = float(input('Digite um valor de dinheiro: '))
-            while True:
-                moeda_inicial = int(input('Moeda inicial:\n'
-                                          '[1] - Dólar\n'
-                                          '[2] - Real\n'
-                                          '[3] - Euro\n'
-                                          '[4] - Iene japonês\n'
-                                          'Sua escolha: '))
-                if moeda_inicial not in (1, 2, 3, 4):
-                    print('Moeda inicial inválida.')
-                else:
-                    moeda_inicial = dic_moedas[moeda_inicial]
-                    print(moeda_inicial)
-                    break
-            while True:
-                moeda_cambio = int(input('Moeda de câmbio:\n'
-                                         '[1] - Dólar\n'
-                                         '[2] - Real\n'
-                                         '[3] - Euro\n'
-                                         '[4] - Iene japonês\n'
-                                         'Sua escolha: '))
-                if moeda_cambio not in (1, 2, 3, 4):
-                    print('Moeda de câmbio inválida.')
-                else:
-                    moeda_cambio = dic_moedas[moeda_cambio]
-                    print(moeda_cambio)
-                    break
-            conversao = float(requisicao[moeda_inicial+moeda_cambio]["bid"]) * valor
-            print(f'A conversão de {moeda_inicial} para {moeda_cambio} é: {conversao:.2f}')
-            break
+            try:
+                valor = float(input('Digite um valor de dinheiro: '))
+                while True:
+                    print('-' * 50)
+                    moeda_inicial = int(input('Moeda inicial:\n'
+                                              '[1] - Dólar\n'
+                                              '[2] - Real\n'
+                                              '[3] - Euro\n'
+                                              '[4] - Iene japonês\n'
+                                              'Sua escolha: '))
+                    if moeda_inicial not in (1, 2, 3, 4):
+                        print('Moeda inicial inválida.')
+                    else:
+                        # Pega a sigla da moeda inicial escolhida, de acordo com o dicionário de moedas.
+                        moeda_inicial = dic_moedas[moeda_inicial]
+                        # print(moeda_inicial)
+                        break
+                while True:
+                    print('-' * 50)
+                    moeda_cambio = int(input('Moeda de câmbio:\n'
+                                             '[1] - Dólar\n'
+                                             '[2] - Real\n'
+                                             '[3] - Euro\n'
+                                             '[4] - Iene japonês\n'
+                                             'Sua escolha: '))
+                    if moeda_cambio not in (1, 2, 3, 4):
+                        print('Moeda de câmbio inválida.')
+                    else:
+                        # Pega a sigla da moeda de câmbio escolhida, de acordo com o dicionário de moedas..
+                        moeda_cambio = dic_moedas[moeda_cambio]
+                        # print(moeda_cambio)
+                        break
+                # concatenação das siglas da moeda inicial + moeda de câmbio, na posição "bid" (cotação), * o valor
+                # escolhido pelo usuário.
+                conversao = float(requisicao[moeda_inicial+moeda_cambio]["bid"]) * valor
+                print(f'A conversão de {valor} {moeda_inicial} para {moeda_cambio} é: {conversao:.2f}')
+                break
+            except (ValueError, TypeError):
+                print('Erro no tipo ou valor.')
+            except Exception as causa:
+                print(f'Ocorreu algum erro: {causa.__cause__}')
     elif escolha_usuario == '3':
         print('Encerrado.')
         break
